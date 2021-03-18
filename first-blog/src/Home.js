eb0606f9e7a84a64750e9f05bf63317f4a1e6474
tGitm <timrus99@gmail.com>
2021-03-18 17:17:24 +0100

    Outputting lsit with JS map method

--- first-blog/src/Home.js
+++ first-blog/src/Home.js
@@ -1,22 +1,23 @@
 import { useState } from 'react';
 
 const Home = () => {
-
-    //let name = 'Tim';
-    const [name, setName] = useState('Tim');
-    const [age, setAge] = useState(21);
-
-    const handleClick = () => {
-        setName('Bojan');
-        setAge(22);
-    }
+    const [blogs, setBlogs] = useState(
+      [
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ]
+    );
+  
 
     return (
       <div className="home">
-        <h2>Homepage</h2>
-        <p>{name} is {age} years old.</p>
-        <button onClick={handleClick}>Change Name and Age</button>
-        <button onClick={() => setName('Tim')}>Reset Name</button>
+        {blogs.map((blog) => (  //loop čez seznam in outputa list
+          <div className="blog-preview" key={blog.id}> {/*react rabi key, da tracka items*/}
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}.</p>
+          </div>
+        ))}
       </div>
     );
   }
